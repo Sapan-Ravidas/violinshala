8caa46ed1c0fd0a316cc3b499c23ae300db17e7e
moonstoper <sjkachhap@outlook.com>
2020-10-08 00:33:21 +0530

    working new header

--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<div class="navbar navbar-inverse navbar-fixed-top">
+<div class="navbar navbar-inverse navbar-fixed-top navig">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-target="#myNavbar" data-toggle="collapse">
@@ -6,13 +6,13 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a href="index.php" class="navbar-brand">Violinshala</a>
+            <a href="index.php" class="navbar-brand logo" style="color:white;">Violinshala</a>
         </div>
         <div class="navbar-collapse collapse" id="myNavbar">
             <ul class="nav navbar-nav navbar-right">
                 <?php
                 if(isset($_SESSION['email'])){?>
-                    <li><a href="info.php"><span class="glyphicon glyphicon-book"></span> About</a></li>
+                    <li class="nav-links"><a href="info.php"><span class="glyphicon glyphicon-book X"></span> About</a></li>
                     <li><a href="lesson.php"><span class="glyphicon glyphicon-education"></span> Lession</a></li>
                     <li><a href="setting.php"><span class="glyphicon glyphicon-cog"></span> Settings</a></li>
                     <li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
